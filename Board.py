6a3e94774336979833a3c6064ec7dccfb6641758
LucasLCO <lucaslco2005@gmail.com>
2023-12-12 14:48:21 -0300

    feat[Board]: add tile getter

--- Board.py
+++ Board.py
@@ -3,7 +3,15 @@ from Tile import Tile
 
 class Board:
     def __init__(self) -> None:
-        self.tiles = [[Tile(x, y)
+        self._tiles = [[Tile(x, y)
                   for x in range(9)]
                   for y in range(9)]
-        self.pieces = None
+        self._pieces = {"black":[],
+                       "white":[]}
+
+    @property
+    def tiles(self):
+        return [row[:] for row in self._tiles]
+
+    def get_tile(self, coordx, coordy):
+        return self._tiles[coordy][coordx]
