4d40d25c036d04cd3844df37298c687aeac40185
LucasLCO <lucaslco2005@gmail.com>
2023-12-13 10:10:44 -0300

    feat[Move]: add Move initial body

--- Move.py
+++ Move.py
@@ -1,3 +1,15 @@
+from dataclasses import dataclass, field
+from Tile import Tile
+
+
+def is_atack(where: Tile) -> bool:
+    pass
+
+@dataclass
 class Move:
-    def __init__(self) -> None:
-        pass
+    where: type(Tile)
+    atack: bool = field(init=False)
+    
+    def __post_init__(self):
+        self.atack = is_atack(self.where)
+    
