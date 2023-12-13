072eb5ac798ae7d6a0cc418ef938775be1558f4d
LucasLCO <lucaslco2005@gmail.com>
2023-12-13 10:10:03 -0300

    feat[Piece]: add pawn class

--- Piece.py
+++ Piece.py
@@ -1,14 +1,23 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from Tile import Tile
+from Move import Move
 
 
 @dataclass
 class Piece(ABC):
     tile: type(Tile)
     white: bool
-    value: int
+    value: int = field(init=False)
 
     @abstractmethod
-    def possible_movement(self):
+    def possible_movement(self) -> tuple:
         pass
+
+@dataclass
+class Pawn(Piece):
+    value: int = field(default=1, init=False, repr=False)
+
+    def possible_movement(self) -> tuple:
+        pass
+
