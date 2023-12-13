08acbf629cd3925267c23751988b7d3355ac987e
LucasLCO <lucaslco2005@gmail.com>
2023-12-13 14:09:07 -0300

    feat[Piece]: add all pieces factorys and classes

--- Piece.py
+++ Piece.py
@@ -36,3 +36,59 @@ class PawnFactory(PiecesFactory):
 
     def get_black_piece(self, tile:Tile) -> Piece:
         return Pawn(tile, False)
+    
+@dataclass
+class Knight(Piece):
+    value: int = field(default=3, init=False, repr=False)
+
+    def possible_movement(self) -> tuple:
+        pass
+
+class KnightFactory(PiecesFactory):
+    def get_white_piece(self, tile:Tile) -> Piece:
+        return Knight(tile, True)
+
+    def get_black_piece(self, tile:Tile) -> Piece:
+        return Knight(tile, False)
+    
+@dataclass
+class Bishop(Piece):
+    value: int = field(default=3, init=False, repr=False)
+
+    def possible_movement(self) -> tuple:
+        pass
+
+class BishopFactory(PiecesFactory):
+    def get_white_piece(self, tile:Tile) -> Piece:
+        return Bishop(tile, True)
+
+    def get_black_piece(self, tile:Tile) -> Piece:
+        return Bishop(tile, False)
+    
+@dataclass
+class Rook(Piece):
+    value: int = field(default=5, init=False, repr=False)
+
+    def possible_movement(self) -> tuple:
+        pass
+
+class RookFactory(PiecesFactory):
+    def get_white_piece(self, tile:Tile) -> Piece:
+        return Rook(tile, True)
+
+    def get_black_piece(self, tile:Tile) -> Piece:
+        return Rook(tile, False)
+
+@dataclass
+class Queen(Piece):
+    value: int = field(default=9, init=False, repr=False)
+
+    def possible_movement(self) -> tuple:
+        pass
+
+class QueenFactory(PiecesFactory):
+    def get_white_piece(self, tile:Tile) -> Piece:
+        return Queen(tile, True)
+
+    def get_black_piece(self, tile:Tile) -> Piece:
+        return Queen(tile, False)
