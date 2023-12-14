4639f52293f3a79f07633d8e84f7a4d545eae2ef
LucasLCO <lucaslco2005@gmail.com>
2023-12-14 13:35:34 -0300

    feat[Board]: create tiles and board factory

--- Board.py
+++ Board.py
@@ -1,13 +1,28 @@
+from dataclasses import dataclass, field
+from Piece import PieceFactory
+from typing import Type
 from Tile import Tile
+# , tiles, pieces
+# The tiles and picies should not be
+# a responsability to Board create.
+# make an BoardBuilder and pass tiles
+# and picies as reference on the __init__
 
+        # self._tiles = [[Tile(x, y)
+        #           for x in range(9)]
+        #           for y in range(9)]
+        # self._pieces = {"black":[],
+        #                "white":[]}
+
+def tiles_factory(tile: Type[Tile]):
+    return [[tile(x, y)
+            for x in range(9)]
+            for y in range(9)]
 
 class Board:
-    def __init__(self) -> None:
-        self._tiles = [[Tile(x, y)
-                  for x in range(9)]
-                  for y in range(9)]
-        self._pieces = {"black":[],
-                       "white":[]}
+    def __init__(self, tiles: list, pieces: dict) -> None:
+        self._tiles = tiles
+        self._pieces = pieces
 
     @property
     def tiles(self):
@@ -15,3 +30,15 @@ class Board:
 
     def get_tile(self, coordx, coordy):
         return self._tiles[coordy][coordx]
+
+@dataclass
+class BoardFactory:
+    tiles: list = field(init=False, repr=False)
+    piece_factory: Type[PieceFactory] = field(init=False, repr=False,
+                                            default_factory=PieceFactory)
+
+    def __post_init__(self):
+        self.tiles = tiles_factory(Tile)
+
+    def __call__(self):
+        pass
